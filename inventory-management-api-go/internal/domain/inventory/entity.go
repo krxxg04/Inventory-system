59bee92a3018d96018c7951159c3947ede2e9138
krxxg <u202312629@upc.edu.pe>
2026-05-19 23:24:11 -0500

    feat: Añadir relación de producto en la entidad de movimiento de inventario y categoría en la entidad de producto.

--- inventory-management-api-go/internal/domain/inventory/entity.go
+++ inventory-management-api-go/internal/domain/inventory/entity.go
@@ -1,6 +1,10 @@
 package inventory
 
-import "time"
+import (
+	"time"
+
+	"inventory-management-api-go/internal/domain/product"
+)
 
 type MovementType string
 
@@ -10,10 +14,11 @@ const (
 )
 
 type InventoryMovement struct {
-	ID          uint         `gorm:"primaryKey"`
-	ProductID   uint         `gorm:"not null;index"`
-	Type        MovementType `gorm:"size:20;not null;index"`
-	Quantity    int64        `gorm:"not null;check:quantity > 0"`
-	Description string       `gorm:"size:255"`
+	ID          uint            `gorm:"primaryKey"`
+	ProductID   uint            `gorm:"not null;index"`
+	Product     product.Product `gorm:"constraint:OnUpdate:RESTRICT,OnDelete:RESTRICT;foreignKey:ProductID;references:ID"`
+	Type        MovementType    `gorm:"size:20;not null;index"`
+	Quantity    int64           `gorm:"not null;check:quantity > 0"`
+	Description string          `gorm:"size:255"`
 	CreatedAt   time.Time
 }
--- inventory-management-api-go/internal/domain/product/entity.go
+++ inventory-management-api-go/internal/domain/product/entity.go
@@ -1,15 +1,20 @@
 package product
 
-import "time"
+import (
+	"time"
+
+	"inventory-management-api-go/internal/domain/category"
+)
 
 type Product struct {
-	ID          uint    `gorm:"primaryKey"`
-	Name        string  `gorm:"size:150;not null"`
-	Description string  `gorm:"size:255"`
-	SKU         string  `gorm:"size:80;not null;uniqueIndex"`
-	Price       float64 `gorm:"type:numeric(12,2);not null;check:price >= 0"`
-	Stock       int64   `gorm:"not null;default:0;check:stock >= 0"`
-	CategoryID  uint    `gorm:"not null;index"`
+	ID          uint              `gorm:"primaryKey"`
+	Name        string            `gorm:"size:150;not null"`
+	Description string            `gorm:"size:255"`
+	SKU         string            `gorm:"size:80;not null;uniqueIndex"`
+	Price       float64           `gorm:"type:numeric(12,2);not null;check:price >= 0"`
+	Stock       int64             `gorm:"not null;default:0;check:stock >= 0"`
+	CategoryID  uint              `gorm:"not null;index"`
+	Category    category.Category `gorm:"constraint:OnUpdate:RESTRICT,OnDelete:RESTRICT;foreignKey:CategoryID;references:ID"`
 	CreatedAt   time.Time
 	UpdatedAt   time.Time
 }
